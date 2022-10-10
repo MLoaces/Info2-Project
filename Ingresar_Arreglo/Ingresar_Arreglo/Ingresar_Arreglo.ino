a0eec63cf68546057f0f689c09dd2fd89b346f31
NicoAvedano <113243565+NicoAvedano@users.noreply.github.com>
2022-10-10 19:00:16 -0300

    Funciones Ingresar Mostrar Arreglo

---
+++ Ingresar_Arreglo/Ingresar_Arreglo/Ingresar_Arreglo.ino
@@ -0,0 +1,30 @@
+const int N=5;
+int a[N];
+void ImprimirArreglo();
+void IngresarArreglo();
+void setup()
+{
+  Serial.begin(9600);
+    IngresarArreglo();
+  ImprimirArreglo();
+}
+
+void loop()
+{
+ 
+}
+void IngresarArreglo(){
+
+  for(int i=0; i<N; i++){
+    Serial.println("Introduce un numero: ");
+   while(Serial.available()<=0){};
+    a[i]=Serial.parseInt();
+  }
+}
+void ImprimirArreglo(){
+  Serial.println("Los numeros son: ");
+  for (int i=0; i<N; i++){
+    Serial.println(a[i]);
+  }
+  Serial.println();
+}
