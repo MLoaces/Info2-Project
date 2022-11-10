aca40a9d0427dc4096715e07b185a33ef11253de
NicoAvedano <113243565+NicoAvedano@users.noreply.github.com>
2022-11-10 17:10:59 -0300

    indentado y cambios en 5.0

--- Proyecto_Main5.0__intervalo_/Proyecto_Main5.0__intervalo_.ino
+++ Proyecto_Main5.0__intervalo_/Proyecto_Main5.0__intervalo_.ino
@@ -3,21 +3,24 @@
 #include <ThreeWire.h>  
 #include <RtcDS1302.h>
 #include <LiquidCrystal_I2C.h>
-
-const int analogInPin = A1;
-const int OutPin = 13; 
-int sensorValue = 0; 
-
-const int N=2;
 #define boton A5
 #define buzzer 9
+#define analogInPin A1
+#define OutPin 13
+#define N 2
+
+/*const int analogInPin = A1;
+const int OutPin = 13; 
+const int N=2;*/
+
+int sensorValue = 0; 
 int a[N];
+int seconds = 10;
+int k=0;
+
 void ImprimirArreglo();
 void IngresarArreglo();
 void Menu();
-void ImprimirResultados();
-int seconds = 10;
-int k=0;
 
 LiquidCrystal_I2C lcd_1(0x27, 16, 2);
 ThreeWire myWire(4,5,2); // IO, SCLK, CE
@@ -25,26 +28,26 @@ RtcDS1302<ThreeWire> Rtc(myWire);
 
 void setup()
 {
-
   lcd_1.setBacklight(1);
+ 
   pinMode(buzzer, OUTPUT);
   pinMode(OutPin, OUTPUT);
   pinMode(analogInPin, INPUT);
   pinMode(boton, INPUT);
+  
   Serial.begin(9600);
+ 
   Rtc.Begin();
   lcd_1.init();
   lcd_1.clear();
   lcd_1.print(" Reloj Largador");
   lcd_1.setCursor(0, 1);
   lcd_1.print("Ver Puerto Serie");
+  
   Menu();
-  //IngresarArreglo();
-  //ImprimirArreglo();
 
   lcd_1.clear();
   lcd_1.print("Regresion: ");
-
 
 }
 
@@ -54,65 +57,72 @@ void loop()
     int j=0;
     lcd_1.clear();
     lcd_1.print(" Ver Resultados");
+    
     while(j!=N){
-    sensorValue = analogRead(analogInPin);
-    if(sensorValue > 900)
-     {
-       digitalWrite(OutPin, HIGH);
-       RtcDateTime now = Rtc.GetDateTime();
-       Serial.println("Ingrese el Numero del piloto que finalizo: ");
-       while(Serial.available()==0){};
-       String str = Serial.readStringUntil('\n');
-       int piloto = str.toInt();
-       Serial.print("Piloto Numero: ");Serial.print(piloto);Serial.print(" Hora de llegada: ");
-       printDateTime(now);
-       j++;
-       delay(1000);
-     }
-    else
-    {
-      digitalWrite(OutPin, LOW);
-    }
+      sensorValue = analogRead(analogInPin);
+      if(sensorValue > 900)
+        {
+          digitalWrite(OutPin, HIGH);
+          RtcDateTime now = Rtc.GetDateTime();
+          Serial.println("Ingrese el Numero del piloto que finalizo: ");
+       
+          while(Serial.available()==0){};
+          String str = Serial.readStringUntil('\n');
+          int piloto = str.toInt();
+       
+          Serial.print("Piloto Numero: ");Serial.print(piloto);Serial.print(" Hora de llegada: ");
+          printDateTime(now);
+          j++;
+          delay(1000);
+        }
+      else
+       {
+          digitalWrite(OutPin, LOW);
+       }
     }
     while(1){}
    }
+  
   Serial.print("Piloto Numero: ");Serial.print(a[k]);Serial.print(" Hora de largada: ");
   RtcDateTime now = Rtc.GetDateTime();
   printDateTime(now);
+  
   lcd_1.setCursor(0, 1);
   lcd_1.print("Corredor N: ");
   lcd_1.print(a[k]);
   lcd_1.setCursor(0, 0);
-  for(int i=10; i>=0; i--){
-  if (seconds>=10){
-    lcd_1.setCursor(13, 0);
-    lcd_1.print(seconds);
-  }
-  if (seconds<10){
-    lcd_1.setCursor(13, 0); 
-    lcd_1.print(0);
-    lcd_1.setCursor(14, 0);
-    lcd_1.print(seconds);
-  }
   
-  delay(900);
-  seconds--;
+  for(int i=10; i>=0; i--){
+    if (seconds>=10){
+      lcd_1.setCursor(13, 0);
+      lcd_1.print(seconds);
+    }
+    if (seconds<10){
+      lcd_1.setCursor(13, 0); 
+      lcd_1.print(0);
+      lcd_1.setCursor(14, 0);
+      lcd_1.print(seconds);
+    }
+  
+    delay(900);
+    seconds--;
  
-  if (seconds<=3){
-  tone(buzzer, 300, 500);
-}
-  if (seconds==0){
-  tone(buzzer, 900, 800);
-  seconds=10;
-}
+    if (seconds<=3){
+      tone(buzzer, 300, 500);
+    }
+    if (seconds==0){
+      tone(buzzer, 900, 800);
+      seconds=10;
+    }
   }
   k+=1;
 }
+
 void IngresarArreglo(){
 
   for(int i=0; i<N; i++){
     Serial.println("Introduce un numero: ");
-   while(Serial.available()==0){};
+    while(Serial.available()==0){};
     String str = Serial.readStringUntil('\n');
     a[i] = str.toInt();
   }
@@ -129,22 +139,18 @@ void ImprimirArreglo(){
 void Menu(){
   int i=0;
   do{
-  Serial.println("\nBienvenido al menu\n1.Ingresar corredores\n2.Mostrar Lista\n3.Guardar\n");
-  while(Serial.available()==0){;};
-  String str = Serial.readStringUntil('\n');
-  i = str.toInt();
-  if (i==1){IngresarArreglo();}
-  if (i==2){ImprimirArreglo();}
-  if (i==3){Serial.println("Reloj listo para comenzar, Presione el boton rojo\n"); while(analogRead(boton)>1000){;}}
+    Serial.println("\nBienvenido al menu\n1.Ingresar corredores\n2.Mostrar Lista\n3.Guardar\n");
+    while(Serial.available()==0){;};
+    String str = Serial.readStringUntil('\n');
+    i = str.toInt();
+    if (i==1){IngresarArreglo();}
+    if (i==2){ImprimirArreglo();}
+    if (i==3){Serial.println("Reloj listo para comenzar, Presione el boton rojo\n"); while(analogRead(boton)>1000){;}}
+    
   } while(i!=3);
 }
 
-/*void ImprimirResultados(){
-  Serial.println("\tResultados de la carrera\n\n");
-  RtcDateTime now = Rtc.GetDateTime();
-  printDateTime(now);
 
-} */
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 void printDateTime(const RtcDateTime& dt)
 {
@@ -158,9 +164,10 @@ void printDateTime(const RtcDateTime& dt)
             dt.Second() );
     Serial.println(datestring);
 }
-void restaHoras(const RtcDateTime& dt, const RtcDateTime& dt1)
+
+/*void restaHoras(const RtcDateTime& dt, const RtcDateTime& dt1)
 {
     char datestring[20];
 
     Serial.println(datestring);
-}
+}*/
